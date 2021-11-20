65ab84072bd2335ccbf8066967e7af2087b45e5b
Mikal Morello <mikal_morello@harvard.edu>
2021-11-20 15:50:02 -0500

    Test API call to Django app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function getStockpile() {
+  console.log("test api button");
+  let apiUrl = "http://127.0.0.1:8000/api/stockpiles";
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Stockpile</h1>
       </header>
+      <main>
+        <button onClick={() => getStockpile()}>test api</button>
+      </main>
     </div>
   );
 }
