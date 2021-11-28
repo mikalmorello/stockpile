8cecc6d747978e2d15836fa9d464d4bcaee390f2
Mikal Morello <mikal_morello@harvard.edu>
2021-11-27 23:01:12 -0500

    Add react-select npm package, add select field to stockpile form

--- src/views/StockpileAdd/index.js
+++ src/views/StockpileAdd/index.js
@@ -1,12 +1,26 @@
 // Dependencies
 import React from "react";
+import Select from "react-select";
+
+// Select options
+const options = [
+  { value: "AAON", label: "AAON" },
+  { value: "AAOI", label: "AAOI" },
+  { value: "AGFY", label: "AGFY" },
+];
 
 function StockpileAdd() {
   // State
-  const [title, setTitle] = React.useState("");
+  const [title, setTitle] = React.useState(""),
+    [selectedOption, setSelectedOption] = React.useState();
 
-  // Create stockpile
-  function handleSubmit(e) {
+  // Handle select
+  const onchangeSelect = (item) => {
+    setSelectedOption(item);
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
     console.log("create stockpile");
 
     // Prevent default form submission
@@ -17,16 +31,23 @@ function StockpileAdd() {
       method: "POST",
       body: JSON.stringify({
         title: title,
+        stocks: selectedOption.value,
       }),
     });
-  }
+  };
 
   return (
     <main>
       <h1>Add stockpile</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input id="title" name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input id="title" name="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        <Select
+          value={selectedOption}
+          onChange={onchangeSelect}
+          options={options}
+          //
+        />
         <button type="submit">Submit</button>
       </form>
     </main>
