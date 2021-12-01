777c68cd196165cb5b63468b338b0fd8f5f761f2
Mikal Morello <mikal_morello@harvard.edu>
2021-11-30 23:36:06 -0500

    Add basic UserContext to all routes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./views/App";
@@ -9,32 +10,36 @@ import User from "./views/User";
 import Stocks from "./views/Stocks";
 import Stock from "./views/Stock";
 import Symbols from "./views/Symbols";
+import UserContext from "./context/UserContext";
 
 const rootElement = document.getElementById("root");
+
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/stockpiles" element={<Stockpiles />}>
-        <Route path=":stockpileId" element={<Stockpile />} />
-      </Route>
-      <Route path="/stockpiles/add" element={<StockpileAdd />} />
-      <Route
-        path="*"
-        element={
-          <main>
-            <h1>There's nothing here!</h1>
-          </main>
-        }
-      />
-      <Route path="/users" element={<Users />}>
-        <Route path=":userId" element={<User />} />
-      </Route>
-      <Route path="/stocks" element={<Stocks />}>
-        <Route path=":stockSymbol" element={<Stock />} />
-      </Route>
-      <Route path="/symbols" element={<Symbols />} />
-    </Routes>
-  </BrowserRouter>,
+  <UserContext.Provider value="testing">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/stockpiles" element={<Stockpiles />}>
+          <Route path=":stockpileId" element={<Stockpile />} />
+        </Route>
+        <Route path="/stockpiles/add" element={<StockpileAdd />} />
+        <Route
+          path="*"
+          element={
+            <main>
+              <h1>There's nothing here!</h1>
+            </main>
+          }
+        />
+        <Route path="/users" element={<Users />}>
+          <Route path=":userId" element={<User />} />
+        </Route>
+        <Route path="/stocks" element={<Stocks />}>
+          <Route path=":stockSymbol" element={<Stock />} />
+        </Route>
+        <Route path="/symbols" element={<Symbols />} />
+      </Routes>
+    </BrowserRouter>
+  </UserContext.Provider>,
   rootElement
 );
--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,7 +1,13 @@
+// Dependencies
+import React, { useContext } from "react";
+import UserContext from "../../context/UserContext";
+
 function App() {
+  const msg = useContext(UserContext);
   return (
     <div>
       <h1>App</h1>
+      {msg}
     </div>
   );
 }
